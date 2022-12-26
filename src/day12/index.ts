b79e4cab1fba33be651139cac7db37beeadafa27
IkenoXamos <moberlies1337@gmail.com>
2022-12-25 21:44:53 -0800

    Day 12 Part 1 Solution

--- src/day12/index.ts
+++ src/day12/index.ts
@@ -1,17 +1,17 @@
 import run from "aocrunner";
-import { combinations2 } from "../utils/index.js";
+import { combinations2, ObjectSet } from "../utils/index.js";
 
 type Position = [number, number];
 
 const Directions = {
-  Left: [-1, 0] as Position,
-  Right: [1, 0] as Position,
-  Up: [0, -1] as Position,
-  Down: [0, 1] as Position,
+  Left: [0, -1] as Position,
+  Right: [0, 1] as Position,
+  Up: [-1, 0] as Position,
+  Down: [1, 0] as Position,
 }
 
-function positionIsValid(position: Position, gridSize: number): boolean {
-  return (position[0] >= 0 && position[0] < gridSize) && (position[1] >= 0 && position[1] < gridSize);
+function positionIsValid(position: Position, maxRows: number, maxCols: number): boolean {
+  return (position[0] >= 0 && position[0] < maxRows) && (position[1] >= 0 && position[1] < maxCols);
 }
 
 const parseInput = (rawInput: string) => {
@@ -32,9 +32,9 @@ const parseInput = (rawInput: string) => {
     }
   }
 
-  const moves: { letter: string; validSteps: [Position, number][]; }[][] = [];
+  const moves: { letter: string; validSteps: Position[]; }[][] = [];
   for (let i = 0; i < grid.length; i++) {
-    moves.push(new Array<{ letter: string; validSteps: [Position, number][]; }>(grid[i].length));
+    moves.push(new Array<{ letter: string; validSteps: Position[]; }>(grid[i].length));
     for (let j = 0; j < grid[i].length; j++) {
       const position = [i, j] as Position;
       const letter = grid[position[0]][position[1]];
@@ -42,40 +42,19 @@ const parseInput = (rawInput: string) => {
       const validSteps = Object.values(Directions)
       .map((direction) => [position[0] + direction[0], position[1] + direction[1]] as Position)
       .map((destination) => {
-        if (positionIsValid(destination, grid.length) && canMove(letter, grid[destination[0]][destination[1]])) {
-          return [destination, manhattanDistance(destination, endingPosition)] as [Position, number];
+        if (positionIsValid(destination, grid.length, grid[i].length) && canMove(letter, grid[destination[0]][destination[1]])) {
+          return destination;
         }
 
-        return [destination, -1] as [Position, number];
+        return [-1, -1] as Position;
       })
-      .filter(([, distance]) => (distance !== -1))
-      .sort(([, a], [, b]) => (a - b));
+      .filter(([row, col]) => (row !== -1 || col !== -1));
 
-      console.log(`At [${position[0]}, ${position[1]}]`, letter, validSteps);
+      // console.log(`At [${position[0]}, ${position[1]}]`, letter, validSteps);
 
       moves[i][j] = {letter, validSteps};
     }
   }
-
-  // const something = Array.from(combinations2(grid.length, grid[0].length)).map((position) => {
-  //   const letter = grid[position[0]][position[1]];
-
-  //   const validSteps = Object.values(Directions)
-  //     .map((direction) => [position[0] + direction[0], position[1] + direction[1]] as Position)
-  //     .map((destination) => {
-  //       if (positionIsValid(destination, grid.length) && canMove(letter, grid[destination[0]][destination[1]])) {
-  //         return [destination, manhattanDistance(destination, endingPosition)] as [Position, number];
-  //       }
-
-  //       return [destination, -1] as [Position, number];
-  //     })
-  //     .filter(([, distance]) => (distance !== -1))
-  //     .sort(([, a], [, b]) => (a - b));
-
-  //   console.log(`At [${position[0]}, ${position[1]}]`, letter, validSteps);
-    
-  //   return {position: position as Position, letter, validSteps};
-  // });
 
   return {moves, startingPosition, endingPosition};
 };
@@ -95,20 +74,31 @@ function positionsMatch(a: Position, b: Position): boolean {
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  let position: Position = [...input.startingPosition];
-  let moves = 0;
+  const toVisit: Array<[Position, number]> = new Array([input.startingPosition, 0]);
+  const visited: ObjectSet<Position> = new ObjectSet();
+  
+  while (toVisit.length > 0) {
+    const tuple = toVisit.shift();
 
-  while(!positionsMatch(position, input.endingPosition)) {
-    moves++;
-    const [step, distance] = input.moves[position[0]][position[1]].validSteps[0];
-    console.log(`Move ${moves}: From [${position[0]}, ${position[1]}] to [${step[0]}, ${step[1]}]`);
+    if (!tuple) throw new Error('Failed to grab element from queue');
 
-    position = [...step];
+    const [position, steps] = tuple;
+    visited.add(position);
 
-    if (moves >= 50) break;
+    if (positionsMatch(position, input.endingPosition)) return steps;
+
+    const { validSteps } = input.moves[position[0]][position[1]];
+
+    for (const destination of validSteps) {
+      const destinationWillBeVisited = !!toVisit.find(([p, s]) => (positionsMatch(p, destination) && s === steps + 1));
+
+      if (!visited.has(destination) && !destinationWillBeVisited) {
+        toVisit.push([destination, steps + 1]);
+      }
+    }
   }
 
-  return moves;
+  return -1;
 };
 
 const part2 = (rawInput: string) => {
@@ -142,5 +132,5 @@ run({
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -30,11 +30,11 @@
  *
  */
 
-export class ObjectSet extends Set {
-  add(elem: unknown) {
+export class ObjectSet<T> extends Set<T | string> {
+  add(elem: T) {
     return super.add(typeof elem === 'object' ? JSON.stringify(elem) : elem);
   }
-  has(elem: unknown) {
+  has(elem: T) {
     return super.has(typeof elem === 'object' ? JSON.stringify(elem) : elem);
   }
 }
